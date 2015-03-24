3ad15b1532b9da630362959bfdd5f6a2701db176
Viktor Kjellberg <kjellberg.viktor@gmail.com>
2015-03-24 13:23:47 +0100

    Added page for paging

---
+++ kmom04/zeus/webroot/mdb_paging.php
@@ -0,0 +1,152 @@
+<?php 
+/**
+ * This is a Zeus pagecontroller.
+ *
+ */
+// Include the essential config-file which also creates the $zeus variable with its defaults.
+include(__DIR__.'/config.php');
+
+
+
+/**
+ * Use the current querystring as base, modify it according to $options and return the modified query string.
+ *
+ * @param array $options to set/change.
+ * @param string $prepend this to the resulting query string
+ * @return string with an updated query string.
+ */
+function getQueryString($options, $prepend='?') {
+	// parse query string into array
+	$query = array();
+	parse_str($_SERVER['QUERY_STRING'], $query);
+
+	// Modify the existring query string with new options
+	$query = array_merge($query, $options);
+
+	// Return the modified querystring
+	return $prepend . http_build_query($query);
+}
+
+
+/**
+ * Create links for hits per page.
+ *
+ * @param array $hits a list of hits-options to display.
+ * @return string as a link to this page.
+ */
+function getHitsPerPage($hits) {
+  $nav = "Träffar per sida: ";
+  foreach($hits AS $val) {
+    $nav .= "<a href='" . getQueryString(array('hits' => $val)) . "'>$val</a> ";
+  }  
+  return $nav;
+}
+
+
+
+
+/**
+ * Create navigation among pages.
+ *
+ * @param integer $hits per page.
+ * @param integer $page current page.
+ * @param integer $max number of pages. 
+ * @param integer $min is the first page number, usually 0 or 1. 
+ * @return string as a link to this page.
+ */
+function getPageNavigation($hits, $page, $max, $min=1) {
+  $nav  = "<a href='" . getQueryString(array('page' => $min)) . "'>&lt;&lt;</a> ";
+  $nav .= "<a href='" . getQueryString(array('page' => ($page > $min ? $page - 1 : $min) )) . "'>&lt;</a> ";
+
+  for($i=$min; $i<=$max; $i++) {
+    $nav .= "<a href='" . getQueryString(array('page' => $i)) . "'>$i</a> ";
+  }
+
+  $nav .= "<a href='" . getQueryString(array('page' => ($page < $max ? $page + 1 : $max) )) . "'>&gt;</a> ";
+  $nav .= "<a href='" . getQueryString(array('page' => $max)) . "'>&gt;&gt;</a> ";
+  return $nav;
+}
+
+
+
+// Connect to a MySQL database using PHP PDO
+$dsn      = 'mysql:host=localhost;dbname=Movie;';
+$login    = 'root';
+$password = '';
+$options  = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'");
+try {
+	$pdo = new PDO($dsn, $login, $password, $options);
+}
+catch(Exception $e) {
+	throw new PDOException('Could not connect to database, hiding connection details.');
+}
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+
+
+// Get parameters
+$hits = isset($_GET['hits']) ? strtolower($_GET['hits']) : 8;
+$page = isset($_GET['page']) ? strtolower($_GET['page']) : 1;
+
+
+
+// Check parameters
+is_numeric($hits) or die('Check: Hits must be numeric.');
+is_numeric($page) or die('Check: Page must be numeric.');
+
+
+
+// Get max pages from table, for navigation
+$sql = "SELECT COUNT(id) AS rows FROM VMovie";
+$sth = $pdo->prepare($sql);
+$sth->execute();
+$res = $sth->fetchAll();
+
+
+
+// Get maximal pages
+$max = ceil($res[0]->rows / $hits);
+
+
+
+// Get data
+$sql = "SELECT * FROM VMovie LIMIT $hits OFFSET " . (($page - 1) * $hits);
+$sth = $pdo->prepare($sql);
+$sth->execute();
+$res = $sth->fetchAll();
+
+
+
+// Put results into a HTML-table
+$tr = "<tr><th>Rad</th><th>Id</th><th>Bild</th><th>Titel</th><th>År</th><th>Genre</th></tr>";
+foreach($res AS $key => $val) {
+  $tr .= "<tr><td>{$key}</td><td>{$val->id}</td><td><img width='80' height='40' src='{$val->image}' alt='{$val->title}' /></td><td>{$val->title}</td><td>{$val->YEAR}</td><td>{$val->genre}</td></tr>";
+}
+
+
+
+// Do it and store it all in variables in the Zeus container.
+$zeus['title'] = "Paginering";
+
+$hitsPerPage = getHitsPerPage(array(2, 4, 8));
+$navigatePage = getPageNavigation($hits, $page, $max);
+
+$zeus['main'] = <<<EOD
+<h1>{$zeus['title']}</h1>
+<p>Resultatet från SQL-frågan:</p>
+<p><code>{$sql}</code></p>
+<div class='dbtable'>
+  <div class='rows'>{$hitsPerPage}</div>
+  <table>
+  {$tr}
+  </table>
+  <div class='pages'>{$navigatePage}</div>
+</div>
+EOD;
+
+
+
+// Finally, leave it all to the rendering phase of Zeus.
+include(ZEUS_THEME_PATH);
+
+
